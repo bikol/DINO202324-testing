452a29ce0b36ea7e0c42bdf183a29f7b3c7c7c3f
Patryk Żywica <bikol@amu.edu.pl>
2023-11-29 10:37:42 +0100

    Initial + new project

---
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawanie.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.ino.dodawanie;
+
+/**
+ *
+ * @author bikol
+ */
+public class Dodawanie {
+    public static String dodaj(String a, String b){
+        return "";
+    }
+}
