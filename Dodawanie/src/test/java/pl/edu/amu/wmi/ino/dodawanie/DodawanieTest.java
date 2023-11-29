57b8457cd2540d7cb48bada39e3d4f0a16ebb3f4
Patryk Żywica <bikol@amu.edu.pl>
2023-11-29 10:40:31 +0100

    dodanie testów

---
+++ Dodawanie/src/test/java/pl/edu/amu/wmi/ino/dodawanie/DodawanieTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.ino.dodawanie;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author bikol
+ */
+public class DodawanieTest {
+    
+    public DodawanieTest() {
+    }
+
+    @Test
+    public void testDodaj() {
+        System.out.println("dodaj");
+        String a = "1";
+        String b = "1";
+        String expResult = "2";
+        String result = Dodawanie.dodaj(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testDodaj2() {
+        System.out.println("dodaj2");
+        String a = "0";
+        String b = "0";
+        String expResult = "0";
+        String result = Dodawanie.dodaj(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
