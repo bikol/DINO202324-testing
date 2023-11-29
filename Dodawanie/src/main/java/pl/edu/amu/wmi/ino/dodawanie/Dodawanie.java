4f307bb221f2a900018e93af0e617539add0b9c6
Patryk Olszewski <s481810@labs.wmi.amu.edu.pl>
2023-11-29 10:56:49 +0100

    feat: Implemented

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawanie.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/Dodawanie.java
@@ -11,6 +11,12 @@ package pl.edu.amu.wmi.ino.dodawanie;
  */
 public class Dodawanie {
     public static String dodaj(String a, String b){
+        
+        if(a == "Pajton")
+            return "Nie dotykaj Pajtona, bo cie ugryzie.";
+        else if(a=="" && b=="")
+            return"Dodawanie – wspólna nazwa różnych działań matematycznych, zdefiniowanych na różnych zbiorach i klasach, m.in. tych liczbowych, ale też dla wektorów, niektórych funkcji i części innych zbiorów. ";
+            
         return ""+(Integer.parseInt(b) + Integer.parseInt(a));
     }
 }
